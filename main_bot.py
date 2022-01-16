fb93ccd88b2e43339e9d642527f1bfbf8b490f69
bunny-k28 <armandevilk282004@gmail.com>
2022-01-16 13:33:06 +0530

    Initial commit

---
+++ main_bot.py
@@ -0,0 +1,39 @@
+"""
+Bot ID- .GIt#8078
+"""
+
+
+print('\n\nACTION: Importing required data... STATUS: ', end='')
+import datetime as dt
+import os
+from dotenv import load_dotenv
+
+import discord
+from discord import Colour as c
+from discord.utils import get
+
+# import sqlite3 as sql
+# from time import sleep as wait
+
+
+# import Data.cmds_info as ci
+# from Data.quantam import *
+# from Data.quantam import showTable
+
+print('Done')
+
+# ===========================================================================
+
+load_dotenv()
+
+TOKEN = os.getenv('TOKEN')
+
+bot = discord.Client()
+
+
+@bot.event
+async def on_ready():
+    print('Bot is online...')
+    
+
+bot.run(TOKEN)
