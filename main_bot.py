cbbd7b3502b87b30b7523bf249adff72805acf70
bunny-k28 <armandevilk282004@gmail.com>
2022-01-17 18:08:56 +0530

    added cmd channel check feature

--- main_bot.py
+++ main_bot.py
@@ -49,76 +49,88 @@ async def on_message(msg):
         
         if (checkCMD(_cmd) is True) or (msg.content == '$reboot$'):
             
-            if (msg.author == bot.user) or (str(msg.author) == 'MEE6#4876'):
-                pass
+            if str(msg.channel.name) in all_channels:
+                mbed = bc_Embed('**channel not suppoerted for commands**', f'**Rule** :eight: \n{server_rules[6]}')
+                mbed.set_author(name=msg.author, icon_url=msg.author.avatar_url)
 
-
-            elif msg.content == '.ping':  # done
-                mbed = lc_Embed("**Ping!**", f'**```Current bot ping: {round(bot.latency * 1000)}ms```**')
                 await msg.channel.send(embed=mbed)
-
-
-            elif msg.content.startswith('.help'):
-                try:
-                    with open('Data\\GitHelpFiles\\git_help_cmds.txt', 'r') as git_help_file1:
-                        file_data1 = git_help_file1.read()
-
-                    with open('Data\\GitHelpFiles\\git_help_cmds2.txt', 'r') as git_help_file2:
-                        file_data2 = git_help_file2.read()
-
-                except IOError as IOE:
-                    mbed = error_Embed(msg, IOE)
-                    await bot.get_channel(932192413026512936).send(embed=mbed)
+                wait(5.0)
+                await msg.channel.purge(limit=2)
                 
-                try:
-                    await msg.channel.send(f'{file_data1}\n{file_data2}')
-                    
-                except Exception as E:
-                    mbed = error_Embed(msg, E)
-                    await bot.get_channel(932192413026512936).send(embed=mbed)
+            else:
                 
-
-            elif msg.content.startswith('.commands'):
-                with open('Data\\bot_cmds.txt', 'r') as bot_cmd_file:
-                    file_data = bot_cmd_file.read()
-
-                await msg.channel.send(file_data)
-
-
-            elif msg.content.startswith('.cc'):  # done
-                limit = int(msg.content.replace('.cc ', '')) + 1
+                error_channel = bot.get_channel(932192413026512936)
                 
-                await msg.channel.purge(limit=limit)        
+                if (msg.author == bot.user) or (str(msg.author) == 'MEE6#4876'):
+                    pass
 
 
-            elif msg.content == '$reboot$':  # done(admin command)
-                try:
-                    with open('Data\\admins.txt', 'r', encoding='utf-8') as admins_file:
-                        admins = admins_file.readlines()
-                        
-                except IOError as IOE:
-                    mbed = error_Embed(msg, IOE)
-                    await bot.get_channel(932192413026512936).send(embed=mbed)
+                elif msg.content == '.ping':  # done
+                    mbed = lc_Embed("**Ping!**", f'**```Current bot ping: {round(bot.latency * 1000)}ms```**')
+                    await msg.channel.send(embed=mbed)
 
-                formatData(admins, '\n')
 
-                member = str(msg.author)
-
-                if member in admins:
-                    await msg.channel.send(f'**```Command Given By: {member}\nPermission: Granted\nAction: Restarting .Git bot...```**')
-
+                elif msg.content.startswith('.help'):
                     try:
-                        os.system('py .\main_bot.py')
+                        with open('Data\\GitHelpFiles\\git_help_cmds.txt', 'r') as git_help_file1:
+                            file_data1 = git_help_file1.read()
 
-                    except OSError as OSE:
+                        with open('Data\\GitHelpFiles\\git_help_cmds2.txt', 'r') as git_help_file2:
+                            file_data2 = git_help_file2.read()
 
-                        mbed = error_Embed(msg, OSE)
-                        await bot.get_channel(932192413026512936).send(embed=mbed)
+                    except IOError as IOE:
+                        mbed = error_Embed(msg, IOE)
+                        await error_channel.send(embed=mbed)
+                    
+                    try:
+                        await msg.channel.send(f'{file_data1}\n{file_data2}')
+                        
+                    except Exception as E:
+                        mbed = error_Embed(msg, E)
+                        await error_channel.send(embed=mbed)
+                    
 
-                else:
-                    await msg.channel.send(f'**```Command Given By: {member}\nPermission: Denied```**')
-                    wait(2.0)
-                    await msg.channel.purge(limit=2)
+                elif msg.content.startswith('.commands'):
+                    with open('Data\\bot_cmds.txt', 'r') as bot_cmd_file:
+                        file_data = bot_cmd_file.read()
+
+                    await msg.channel.send(file_data)
+
+
+                elif msg.content.startswith('.cc'):  # done
+                    limit = int(msg.content.replace('.cc ', '')) + 1
+                    
+                    await msg.channel.purge(limit=limit)        
+
+
+                elif msg.content == '$reboot$':  # done(admin command)
+                    try:
+                        with open('Data\\admins.txt', 'r', encoding='utf-8') as admins_file:
+                            admins = admins_file.readlines()
+                            
+                    except IOError as IOE:
+                        mbed = error_Embed(msg, IOE)
+                        await error_channel.send(embed=mbed)
+
+                    formatData(admins, '\n')
+
+                    member = str(msg.author)
+
+                    if member in admins:
+                        await msg.channel.send(f'**```Command Given By: {member}\nPermission: Granted\nAction: Restarting .Git bot...```**')
+
+                        try:
+                            os.system('py .\main_bot.py')
+
+                        except OSError as OSE:
+
+                            mbed = error_Embed(msg, OSE)
+                            await error_channel.send(embed=mbed)
+
+                    else:
+                        await msg.channel.send(f'**```Command Given By: {member}\nPermission: Denied```**')
+                        wait(2.0)
+                        await msg.channel.purge(limit=2)
 
         else:
             if (msg.author == bot.user) or (str(msg.author) == 'MEE6#4876'):
@@ -127,7 +139,7 @@ async def on_message(msg):
             else:
                 await msg.channel.send(f'**```No such command ->{msg.content}<-. Use .commands command to see the list of availabe commands```**')
                 wait(4.0)
-                await msg.channel.purge(limit=1)
+                await msg.channel.purge(limit=2)
 
     else:
         pass
@@ -135,8 +147,14 @@ async def on_message(msg):
 
 @bot.event
 async def on_member_join(member):
-    pass
-
+    
+    try:
+        role_to_add = get(member.guild.roles, name="Members")
+        await member.add_roles(role_to_add)
+        
+    except Exception as E:
+        mbed = error_Embed('assigning role when member joins', E)
+        await bot.get_channel(932192413026512936).send(embed=mbed)
 
 
 
