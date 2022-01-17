ad89e41203ee924358905db0829fd0019e48ed16
bunny-k28 <armandevilk282004@gmail.com>
2022-01-17 13:46:35 +0530

    added admin auth in restart cmd

--- main_bot.py
+++ main_bot.py
@@ -4,15 +4,13 @@ Bot ID- .GIt#8078
 
 
 print('\n\nACTION: Importing required data... STATUS: ', end='')
-import datetime as dt
 import os
 from dotenv import load_dotenv
+from time import sleep as wait
 
 import discord
-from discord import Colour as c
-from discord.utils import get
 
-from webServer import wakeUp
+from Data.quantam import *
 
 print('Done')
 
@@ -22,7 +20,16 @@ load_dotenv()
 
 TOKEN = os.getenv('TOKEN')
 
-bot = discord.Client()
+
+print('ACTION: preparing bot#8078... STATUS: ', end='')
+
+# code for member intents
+intents = discord.Intents.default()
+intents.members = True
+
+bot = discord.Client(intents=intents)
+
+print('Ready')
 
 
 @bot.event
@@ -32,15 +39,46 @@ async def on_ready():
 
 @bot.event
 async def on_message(msg):
-    
-    if msg.author == bot.user:    # done
+
+    if msg.author == bot.user:
         pass
-    
-    
-    elif msg.content.startswith('.check'):
-        await msg.channel.send('**```Perfectly working```**')
 
 
-wakeUp()
+    elif msg.content == '.ping':
+        mbed = lc_Embed("**Ping!**", f'**```Current bot ping: {round(bot.latency * 1000)}ms```**')
+        await msg.channel.send(embed=mbed)
 
+
+    elif msg.content.startswith('.cc'):
+        limit = int(msg.content.replace('.cc ', ''))
+        
+        await msg.content.purge(limit=limit)        
+
+
+    elif msg.content == '$restart$':  # admin command
+        with open('Data\\admins.txt', 'r') as admins_file:
+            admins = admins_file.readlines()
+
+        formatData(admins, '\n')
+
+        member = str(msg.author)
+
+        if member in admins:
+            await msg.channel.send(f'**```Command Given By: {member}\nPermission: Granted\nAction: Restarting .Git bot...```**')
+
+            try:
+                os.system('py .\main_bot.py')
+
+            except OSError as OSE:
+
+                mbed = error_Embed(msg, OSE)
+                await bot.get_channel(932192413026512936).send(embed=mbed)
+
+        else:
+            await msg.channel.send(f'**```Command Given By: {member}\nPermission: Denied```**')
+            wait(2.0)
+            await msg.channel.purge(limit=2)
+
+
+# wakeUp()
 bot.run(TOKEN)
